5a85a8405b5c71183f2b2e1fbd47bd6be15b1ccf
Weltgeist <narcisse118@gmail.com>
2020-04-05 23:31:51 -0400

    added generateTarget Function

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-
+/**
+ * generate a random integer number between 0-9
+ * @return {Number} randomly chosen number
+ */
+const generateTarget = () => Math.floor(Math.random()*10); 
